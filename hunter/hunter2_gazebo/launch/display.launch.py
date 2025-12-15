adab27d7fc879616e672ca19e4e17a8559799e0e
Miguel Argolo <argolo.mb@gmail.com>
2025-12-15 10:47:31 -0300

    Add rviz config file

--- hunter/hunter2_gazebo/launch/display.launch.py
+++ hunter/hunter2_gazebo/launch/display.launch.py
@@ -55,7 +55,6 @@ def generate_launch_description():
         model_arg,
         rviz_arg,
         joint_state_publisher_node,
-        joint_state_publisher_gui_node,
         robot_state_publisher_node,
         rviz_node
     ])
